878e34c1ea396f6008160b14ef69c9e3176fd1f5
John Wood <john@signalhq.com>
2010-11-30 08:14:57 -0600

    The object returned by a has_many association should act like an array

--- lib/tenacity/module.rb
+++ lib/tenacity/module.rb
@@ -19,12 +19,16 @@ module Tenacity
 
     def t_has_many(association_id, args={})
       redefine_method(association_id) do
+        []
       end
 
       redefine_method("#{association_id}=") do
       end
 
       redefine_method("#{ActiveSupport::Inflector.singularize(association_id.to_s)}_ids") do
+      end
+
+      redefine_method("#{ActiveSupport::Inflector.singularize(association_id.to_s)}_ids=") do
       end
     end
   end
--- test/core/module_test.rb
+++ test/core/module_test.rb
@@ -5,17 +5,29 @@ class ModuleTest < Test::Unit::TestCase
   context "A class with a belongs_to :account association" do
     person = Person.new
 
-    should("should respond to account") { assert person.respond_to?(:account) }
-    should("should respond to account=") { assert person.respond_to?(:account=) }
-    should("should respond to account_id") { assert person.respond_to?(:account_id) }
+    should("respond to account") { assert person.respond_to?(:account) }
+    should("respond to account=") { assert person.respond_to?(:account=) }
+    should("respond to account_id") { assert person.respond_to?(:account_id) }
   end
 
   context "A class with a has_many :transactions association" do
     person = Person.new
 
-    should("should respond to transactions") { assert person.respond_to?(:transactions) }
-    should("should respond to transactions=") { assert person.respond_to?(:transactions=) }
-    should("should respond to transaction_ids") { assert person.respond_to?(:transaction_ids) }
+    should("respond to transactions") { assert person.respond_to?(:transactions) }
+    should("respond to transactions=") { assert person.respond_to?(:transactions=) }
+    should("respond to transaction_ids") { assert person.respond_to?(:transaction_ids) }
+    should("respond to transaction_ids=") { assert person.respond_to?(:transaction_ids=) }
+  end
+
+  context "The object returned by a has_many association" do
+    person = Person.new
+    transactions = person.transactions
+
+    should("respond to <<") { assert transactions.respond_to?(:<<) }
+    should("respond to delete") { assert transactions.respond_to?(:delete) }
+    should("respond to clear") { assert transactions.respond_to?(:clear) }
+    should("respond to empty?") { assert transactions.respond_to?(:empty?) }
+    should("respond to size") { assert transactions.respond_to?(:size) }
   end
 
 end
