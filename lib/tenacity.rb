f11bdc85357648aa48416659f4dffbaea5bf67e5
John Wood <john@signalhq.com>
2010-12-14 09:20:32 -0600

    Remove the logger

--- lib/tenacity.rb
+++ lib/tenacity.rb
@@ -1,4 +1,3 @@
 require 'tenacity/module'
-require 'tenacity/tenacity_logger'
 require 'tenacity/orm_ext/activerecord.rb'
 require 'tenacity/orm_ext/mongo_mapper.rb'
--- lib/tenacity/tenacity_logger.rb
+++
@@ -1,5 +0,0 @@
-class TenacityLogger
-  def self.log(source, message)
-    puts "#{source}: #{message}" if ENV['TENACITY_DEBUG'] == 'true'
-  end
-end
