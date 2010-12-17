b15b9f86fda954c3977d075033d16ff0aa93770c
John Wood <john@signalhq.com>
2010-12-16 20:41:27 -0600

    Renamed "relationships" to "associations"

--- lib/tenacity.rb
+++ lib/tenacity.rb
@@ -1,11 +1,11 @@
 require File.join('active_support', 'inflector')
 
 require File.join(File.dirname(__FILE__), 'tenacity', 'classmethods')
+require File.join(File.dirname(__FILE__), 'tenacity', 'associations', 'belongs_to')
+require File.join(File.dirname(__FILE__), 'tenacity', 'associations', 'has_many')
+require File.join(File.dirname(__FILE__), 'tenacity', 'associations', 'has_one')
 require File.join(File.dirname(__FILE__), 'tenacity', 'orm_ext', 'activerecord')
 require File.join(File.dirname(__FILE__), 'tenacity', 'orm_ext', 'mongo_mapper')
-require File.join(File.dirname(__FILE__), 'tenacity', 'relationships', 'belongs_to')
-require File.join(File.dirname(__FILE__), 'tenacity', 'relationships', 'has_many')
-require File.join(File.dirname(__FILE__), 'tenacity', 'relationships', 'has_one')
 
 module Tenacity
   include ClassMethods
