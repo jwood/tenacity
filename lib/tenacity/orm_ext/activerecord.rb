716029b758d70044f708c85208e7abae2f22b4ac
John Wood <john@signalhq.com>
2010-12-24 08:40:23 -0600

    Make sure the ORMs have been loaded before extending them

--- lib/tenacity/orm_ext/activerecord.rb
+++ lib/tenacity/orm_ext/activerecord.rb
@@ -39,72 +39,78 @@
 #     t.string :wheel_id
 #   end
 #
-module ActiveRecord
-  class Base #:nodoc:
+begin
+  require 'active_record'
 
-    def self._t_find(id)
-      find_by_id(id)
-    end
+  module ActiveRecord
+    class Base #:nodoc:
 
-    def self._t_find_bulk(ids)
-      return [] if ids.nil? || ids.empty?
-      find(:all, :conditions => ["id in (?)", ids])
-    end
-
-    def self._t_find_first_by_associate(property, id)
-      find(:first, :conditions => ["#{property} = ?", id.to_s])
-    end
-
-    def self._t_find_all_by_associate(property, id)
-      find(:all, :conditions => ["#{property} = ?", id.to_s])
-    end
-
-    def self._t_initialize_has_many_association(association_id)
-      after_save { |record| record.class._t_save_associates(record, association_id) }
-    end
-
-    def self._t_initialize_belongs_to_association(association_id)
-      before_save { |record| record.class._t_stringify_belongs_to_value(record, association_id) }
-    end
-
-    def _t_reload
-      reload
-    end
-
-    def _t_clear_associates(association_id)
-      join_table_name = self.class._t_join_table_name(association_id)
-      self.connection.execute("delete from #{join_table_name} where #{self.class._t_my_id_column} = #{self.id}")
-    end
-
-    def _t_associate_many(association_id, associate_ids)
-      join_table_name = self.class._t_join_table_name(association_id)
-      values = associate_ids.map { |associate_id| "(#{self.id}, '#{associate_id}')" }.join(',')
-
-      self.transaction do
-        _t_clear_associates(association_id)
-        self.connection.execute("insert into #{join_table_name} (#{self.class._t_my_id_column}, #{self.class._t_associate_id_column(association_id)}) values #{values}")
+      def self._t_find(id)
+        find_by_id(id)
       end
+
+      def self._t_find_bulk(ids)
+        return [] if ids.nil? || ids.empty?
+        find(:all, :conditions => ["id in (?)", ids])
+      end
+
+      def self._t_find_first_by_associate(property, id)
+        find(:first, :conditions => ["#{property} = ?", id.to_s])
+      end
+
+      def self._t_find_all_by_associate(property, id)
+        find(:all, :conditions => ["#{property} = ?", id.to_s])
+      end
+
+      def self._t_initialize_has_many_association(association_id)
+        after_save { |record| record.class._t_save_associates(record, association_id) }
+      end
+
+      def self._t_initialize_belongs_to_association(association_id)
+        before_save { |record| record.class._t_stringify_belongs_to_value(record, association_id) }
+      end
+
+      def _t_reload
+        reload
+      end
+
+      def _t_clear_associates(association_id)
+        t_join_table_name = self.class._t_join_table_name(association_id)
+        self.connection.execute("delete from #{t_join_table_name} where #{self.class._t_my_id_column} = #{self.id}")
+      end
+
+      def _t_associate_many(association_id, associate_ids)
+        t_join_table_name = self.class._t_join_table_name(association_id)
+        values = associate_ids.map { |associate_id| "(#{self.id}, '#{associate_id}')" }.join(',')
+
+        self.transaction do
+          _t_clear_associates(association_id)
+          self.connection.execute("insert into #{t_join_table_name} (#{self.class._t_my_id_column}, #{self.class._t_associate_id_column(association_id)}) values #{values}")
+        end
+      end
+
+      def _t_get_associate_ids(association_id)
+        t_join_table_name = self.class._t_join_table_name(association_id)
+        rows = self.connection.execute("select #{self.class._t_associate_id_column(association_id)} from #{t_join_table_name} where #{self.class._t_my_id_column} = #{self.id}")
+        ids = []; rows.each { |r| ids << r[0] }; ids
+      end
+
+      private
+
+      def self._t_my_id_column
+        table_name.singularize + '_id'
+      end
+
+      def self._t_associate_id_column(association_id)
+        association_id.to_s.singularize + '_id'
+      end
+
+      def self._t_join_table_name(association_id)
+        association_id.to_s < table_name ? "#{association_id}_#{table_name}" : "#{table_name}_#{association_id}"
+      end
+
     end
-
-    def _t_get_associate_ids(association_id)
-      join_table_name = self.class._t_join_table_name(association_id)
-      rows = self.connection.execute("select #{self.class._t_associate_id_column(association_id)} from #{join_table_name} where #{self.class._t_my_id_column} = #{self.id}")
-      ids = []; rows.each { |r| ids << r[0] }; ids
-    end
-
-    private
-
-    def self._t_my_id_column
-      table_name.singularize + '_id'
-    end
-
-    def self._t_associate_id_column(association_id)
-      association_id.to_s.singularize + '_id'
-    end
-
-    def self._t_join_table_name(association_id)
-      association_id.to_s < table_name ? "#{association_id}_#{table_name}" : "#{table_name}_#{association_id}"
-    end
-
   end
+rescue LoadError
+  # ActiveRecord not available
 end
