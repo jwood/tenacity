fdfb36c6f4fff4035d29a8322c437b126bdbb05a
John Wood <john@signalhq.com>
2011-01-07 16:35:15 -0600

    Rename ivar_name to _t_ivar_name, and use it to get the name of the property containing the associate ids, instead of building the name in has_many.rb as well.

--- lib/tenacity/instance_methods.rb
+++ lib/tenacity/instance_methods.rb
@@ -1,26 +1,26 @@
 module Tenacity
   module InstanceMethods #:nodoc:
 
+    def _t_ivar_name(association)
+      "@_t_" + association.name.to_s
+    end
+
     private
 
     def get_associate(association, params)
       _t_reload
       force_reload = params.first unless params.empty?
-      value = instance_variable_get ivar_name(association)
+      value = instance_variable_get _t_ivar_name(association)
       if value.nil? || force_reload
         value = yield
-        instance_variable_set ivar_name(association), value
+        instance_variable_set _t_ivar_name(association), value
       end
       value
     end
 
     def set_associate(association, associate)
       yield if block_given?
-      instance_variable_set ivar_name(association), associate
-    end
-
-    def ivar_name(association)
-      "@_t_" + association.name.to_s
+      instance_variable_set _t_ivar_name(association), associate
     end
 
   end
