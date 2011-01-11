1110801f8220958f40df943b7b6adf06e0437255
John Wood <john@signalhq.com>
2011-01-11 16:56:35 -0600

    Find fixture files dynamically to clear the fixtures from the databases

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,40 +23,18 @@ require 'tenacity'
 Dir[File.join(File.dirname(__FILE__), 'fixtures', '*.rb')].each { |file| require file }
 
 def setup_fixtures
-  setup_active_record_fixtures
-  setup_mongo_mapper_fixtures
-  setup_mongoid_fixtures
-end
-
-def setup_active_record_fixtures
-  ActiveRecordObject.delete_all
-
-  ActiveRecordCar.delete_all
-  ActiveRecordClimateControlUnit.delete_all
-  ActiveRecordEngine.delete_all
-  ActiveRecordClimateControlUnit.delete_all
-  ActiveRecordNut.delete_all
+  Dir.glob(File.join(File.dirname(__FILE__), 'fixtures', '*.rb')).each do |filename|
+    begin
+      filename =~ /.*\/(.*)\.rb/
+      clazz = Kernel.const_get($1.camelcase)
+      clazz.delete_all if clazz.respond_to?(:delete_all)
+    rescue NameError
+    end
+  end
 
   ActiveRecordCar.connection.execute("delete from active_record_cars_mongo_mapper_wheels")
   ActiveRecordCar.connection.execute("delete from active_record_cars_couch_rest_doors")
   ActiveRecordCar.connection.execute("delete from nuts_and_wheels")
-end
-
-def setup_mongo_mapper_fixtures
-  MongoMapperObject.delete_all
-
-  MongoMapperAshTray.delete_all
-  MongoMapperButton.delete_all
-  MongoMapperCoil.delete_all
-  MongoMapperDashboard.delete_all
-  MongoMapperVent.delete_all
-  MongoMapperWheel.delete_all
-end
-
-def setup_mongoid_fixtures
-  require_mongoid do
-    MongoidAlternator.delete_all
-  end
 end
 
 def setup_couchdb_fixtures
