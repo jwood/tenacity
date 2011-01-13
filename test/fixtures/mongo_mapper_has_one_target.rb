814a589b3ed49f47385a91ed52d60c495401f114
John Wood <john@signalhq.com>
2011-01-12 18:31:54 -0600

    Place all of the mongoid associations with in a require_mongoid block

--- test/fixtures/mongo_mapper_has_one_target.rb
+++ test/fixtures/mongo_mapper_has_one_target.rb
@@ -5,5 +5,5 @@ class MongoMapperHasOneTarget
   t_belongs_to :active_record_object
   t_belongs_to :couch_rest_object
   t_belongs_to :mongo_mapper_object
-  t_belongs_to :mongoid_object
+  require_mongoid { t_belongs_to :mongoid_object }
 end
