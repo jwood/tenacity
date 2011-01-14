6b083340df30ca01b57551e4fa32a57098544a2a
John Wood <john@signalhq.com>
2011-01-13 22:00:31 -0600

    Rewrote classmethods_test to use generic fixtures

--- test/core/classmethods_test.rb
+++ test/core/classmethods_test.rb
@@ -2,53 +2,53 @@ require 'test_helper'
 
 class ClassmethodsTest < Test::Unit::TestCase
 
-  context "A class with a belongs_to :active_record_car association" do
+  context "A class with a belongs_to :mongo_mapper_object association" do
     setup do
       setup_fixtures
-      @wheel = MongoMapperWheel.new
+      @object = MongoMapperHasOneTarget.new
     end
 
-    should("respond to active_record_car") { assert @wheel.respond_to?(:active_record_car) }
-    should("respond to active_record_car=") { assert @wheel.respond_to?(:active_record_car=) }
-    should("respond to active_record_car_id") { assert @wheel.respond_to?(:active_record_car_id) }
+    should("respond to mongo_mapper_object") { assert @object.respond_to?(:mongo_mapper_object) }
+    should("respond to mongo_mapper_object=") { assert @object.respond_to?(:mongo_mapper_object=) }
+    should("respond to mongo_mapper_object_id") { assert @object.respond_to?(:mongo_mapper_object_id) }
   end
 
-  context "A class with a has_one :active_record_climate_control_unit association" do
+  context "A class with a has_one :mongo_mapper_has_one_target association" do
     setup do
       setup_fixtures
-      @dashboard = MongoMapperDashboard.new
+      @object = MongoMapperObject.new
     end
 
-    should("respond to active_record_climate_control_unit") { assert @dashboard.respond_to?(:active_record_climate_control_unit) }
-    should("respond to active_record_climate_control_unit=") { assert @dashboard.respond_to?(:active_record_climate_control_unit=) }
+    should("respond to mongo_mapper_has_one_target") { assert @object.respond_to?(:mongo_mapper_has_one_target) }
+    should("respond to mongo_mapper_has_one_target=") { assert @object.respond_to?(:mongo_mapper_has_one_target=) }
   end
 
-  context "A class with a has_many :active_record_nuts association" do
+  context "A class with a has_many :mongo_mapper_has_many_targets association" do
     setup do
       setup_fixtures
-      @wheel = MongoMapperWheel.new
+      @object = MongoMapperObject.new
     end
 
-    should("respond to active_record_nuts") { assert @wheel.respond_to?(:active_record_nuts) }
-    should("respond to active_record_nuts=") { assert @wheel.respond_to?(:active_record_nuts=) }
-    should("respond to active_record_nut_ids") { assert @wheel.respond_to?(:active_record_nut_ids) }
-    should("respond to active_record_nut_ids=") { assert @wheel.respond_to?(:active_record_nut_ids=) }
+    should("respond to mongo_mapper_has_many_targets") { assert @object.respond_to?(:mongo_mapper_has_many_targets) }
+    should("respond to mongo_mapper_has_many_targets=") { assert @object.respond_to?(:mongo_mapper_has_many_targets=) }
+    should("respond to mongo_mapper_has_many_target_ids") { assert @object.respond_to?(:mongo_mapper_has_many_target_ids) }
+    should("respond to mongo_mapper_has_many_target_ids=") { assert @object.respond_to?(:mongo_mapper_has_many_target_ids=) }
   end
 
   context "The object returned by a has_many association" do
     setup do
       setup_fixtures
-      @wheel = MongoMapperWheel.new
-      @nuts = @wheel.active_record_nuts
+      @object = MongoMapperObject.new
+      @targets = @object.mongo_mapper_has_many_targets
     end
 
-    should("respond to <<") { assert @nuts.respond_to?(:<<) }
-    should("respond to delete") { assert @nuts.respond_to?(:delete) }
-    should("respond to clear") { assert @nuts.respond_to?(:clear) }
-    should("respond to empty?") { assert @nuts.respond_to?(:empty?) }
-    should("respond to size") { assert @nuts.respond_to?(:size) }
-    should("respond to delete_all") { assert @nuts.respond_to?(:delete_all) }
-    should("respond to destroy_all") { assert @nuts.respond_to?(:destroy_all) }
+    should("respond to <<") { assert @targets.respond_to?(:<<) }
+    should("respond to delete") { assert @targets.respond_to?(:delete) }
+    should("respond to clear") { assert @targets.respond_to?(:clear) }
+    should("respond to empty?") { assert @targets.respond_to?(:empty?) }
+    should("respond to size") { assert @targets.respond_to?(:size) }
+    should("respond to delete_all") { assert @targets.respond_to?(:delete_all) }
+    should("respond to destroy_all") { assert @targets.respond_to?(:destroy_all) }
   end
 
 end
