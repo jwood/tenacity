0d1706289cbaefb7f839ecb1955627a2ebe7acb1
John Wood <john@signalhq.com>
2011-07-15 07:23:50 -0500

    Minor formatting change

--- lib/tenacity/errors.rb
+++ lib/tenacity/errors.rb
@@ -1,17 +1,13 @@
 module Tenacity
   # Generic Tenacity exception class.
-  class TenacityError < StandardError
-  end
+  class TenacityError < StandardError; end
 
   # Raised on attempt to update an associate that is instantiated as read only.
-  class ReadOnlyError < TenacityError
-  end
+  class ReadOnlyError < TenacityError; end
 
   # Raised when one of the objects specified in the relationship does not exist in the database
-  class ObjectDoesNotExistError < TenacityError
-  end
+  class ObjectDoesNotExistError < TenacityError; end
 
   # Rasied when an attempt is made to delete an object whose id is in use by an association
-  class ObjectIdInUseError < TenacityError
-  end
+  class ObjectIdInUseError < TenacityError; end
 end
